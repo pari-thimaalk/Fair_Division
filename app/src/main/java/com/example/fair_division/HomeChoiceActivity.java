246205509edd675d43d07fc76da4cc8f2ee91a4e
Parithimaal Karmehan <pk38@illinois.edu>
2024-03-01 16:53:40 -0600

    created repo

---
+++ app/src/main/java/com/example/fair_division/HomeChoiceActivity.java
@@ -0,0 +1,15 @@
+package com.example.fair_division;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeChoiceActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_choice);
+
+        //INSERT FRAGMENTS HERE
+    }
+}
---
+++ app/src/main/java/com/example/fair_division/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.fair_division;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.content.Intent;
+import android.widget.ScrollView;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ScrollView scrollView = findViewById(R.id.scrollView);
+        scrollView.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                if (scrollY > oldScrollY) {
+                    // Scrolled down, perform transition to the next page
+                    // Example: startActivity(new Intent(MainActivity.this, NextActivity.class));
+                    startActivity(new Intent(MainActivity.this, HomeChoiceActivity.class));
+                    overridePendingTransition(R.anim.slide_up, R.anim.slide_down);
+                }
+            }
+        });
+    }
+}
