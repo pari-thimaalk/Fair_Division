96c6964910b7d39ef419c0c7612d42b6e9b5f8f6
Allton274 <adityadora277@gmail.com>
2024-03-04 14:57:18 -0600

    Updated gradle

--- app/src/main/java/com/example/fair_division/PplAdapter.java
+++ app/src/main/java/com/example/fair_division/PplAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -34,6 +35,12 @@ public class PplAdapter extends RecyclerView.Adapter<PplAdapter.ViewHolder> {
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
         holder.getListHolder().setText(pplList.get(position));
+        holder.itemView.findViewById(R.id.edit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Edit clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
